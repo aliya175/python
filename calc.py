2dbc8d9ff15fa7ddb45075cbc40e613918e1759f
aliya175 <94426677+aliya175@users.noreply.github.com>
2022-01-31 16:12:31 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,11 @@
+def addition(x,y):
+    result=x+y
+    return result
+def subtraction(x,y):
+    return(X-y)
+def multiplication(x,y):
+    return(x*y)
+def division(x,y):
+    return float(x/y)
+
+
